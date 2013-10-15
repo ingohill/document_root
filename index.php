b120149af34371cc5bc15f0c6f8d95e38a2ad554
Ingo Hillebrand <ingo@shift8.de>
2013-10-15 16:37:12 +0200

    New Styles & a little bit bug-fixing

--- index.php
+++ index.php
@@ -1,55 +1,11 @@
 <html>
     <head>
         <title>Auswahl</title>
-        <style>
-            body {
-            	margin: 20px;
-            	padding: 0;
-            	background: #f2f2f2;
-                font-family: arial;
-            }
-            .box {
-            	border-top: 5px solid #ff9900;
-            	border-left: 1px solid #ff9900;
-            	border-right: 1px solid #ff9900;
-                border-bottom: 1px solid #ff9900;
-                /* padding: 20px 30px 20px 30px; */
-                margin: 20px 20px 20px 0;
-                background: #999999;
-                float: left;
-            }
-            .box:first-of-type {
-                padding: 20px 30px 20px 30px;
-            }
-            h2 a, h2 {
-                color: #000000;
-                font-size: 13px;
-                 margin: 0;
-                 padding: 30px;
-            }
-            .box h2 a { padding: 0;
-                display: inline-block;
-            }
-            a, a:visited{
-                text-decoration: none;
-            }
-            .box ul {
-                margin-left:0;
-                padding-left:0;
-            }
-            ul li {
-                list-style-type: none;
-                font-size: 11px;
-                margin-left:5px;
-            }
-            ul li a {
-                color: #000000;
-            }
-        </style>
+        <link rel="stylesheet" href="./index.light.css" type="text/css">
     </head>
 
     <body>
-        <div class="box" style="border-color:#999999; background: #e3e3e3;">
+        <div class="utility box">
             <h2>Utilities</h2>
             <ul>
                 <li><a href="http://localhost/MAMP/phpinfo.php">phpInfo</a></li>
@@ -62,22 +18,26 @@
         <?php
 
             function getDirColorHexString($cnt, $i){
-                $onlyGreen = false;
+                $onlyGreen = true;
                 $ing = 256;
                 $step = floor($ing/$cnt);
                 if( $onlyGreen == false ){
                     if( $i < $cnt/2){
-                        $val = dechex(($cnt-$i)*$step);
-                        if( strlen($val) == 1 ) $val = '0' . $val;	
+                        (string)$val = dechex(($cnt-$i)*$step);
+                        if( $i === 0 ) $val = 'FF';
+                        if( strlen($val) === 1 ) $val = '0' . $val;
                         return '#00' . $val . '00';
                     }else{
-                        $val = dechex($i*$step);
-                        if( strlen($val) == 1 ) $val = '0' . $val;	
+                        (string)$val = dechex($i*$step);
+                        if( $i === ($cnt-1) ) $val = 'FF';
+                        if( strlen($val) === 1 ) $val = '0' . $val;
                         return '#' . $val . '0000';
                     }
                 }else{
-                    $val = dechex(($cnt-$i)*$step);
-                    if( strlen($val) == 1 ) $val = '0' . $val;
+                    (string)$val = dechex(($cnt-$i)*$step);
+					if( $i === 0 ) $val = 'FF';
+					if( $i === ($cnt-1) ) $val = '00';
+                    if( strlen($val) === 1 ) $val = '0' . $val;
                     return '#00' . $val . '00';
                 }
             }
@@ -87,7 +47,7 @@
                 while (false !== ($dir = readdir($handle))) {
                     if( is_dir($dir) && substr($dir, 0, 1) != '.' ){
                         if( array_key_exists(fileatime($dir), $dirs) ){
-                            $dirs[fileatime($dir).'_'.$dir] = $dir;	
+                            $dirs[fileatime($dir).'_'.$dir] = $dir;
                         }else{
                             $dirs[fileatime($dir)] = $dir;
                         }
@@ -102,8 +62,8 @@
                 if( is_dir( $dir . '/htdocs')){
                     $url = $dir . '/htdocs';
                 }
-                $wp_admin = is_dir( $url . '/wp-admin') ? (' [ <a class="wp" href="' . $url . '/wp-admin">WP</a> ]') : '';
-                echo '<div class="box" style="border-color: ' . getDirColorHexString(count($dirs), $i) . '"><h2><a href="/' . $url . '">' . $dir . '</a>' . $wp_admin . '</h2></div>'."\n";	
+                $wp_admin = is_dir( $url . '/wp-admin') ? (' [<a class="wp" href="' . $url . '/wp-admin">WP</a>]') : '';
+                echo '<div class="box" style="border-color: ' . getDirColorHexString(count($dirs), $i) . '"><h2><a href="/' . $url . '">' . $dir . '</a>' . $wp_admin . '</h2></div>'."\n";
                 $i++;
             }
         ?>
