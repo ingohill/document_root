e5975f740e363f41a95f2f4294229602e14212a3
Ingo Hillebrand <ingo@shift8.de>
2013-11-08 10:17:32 +0100

    Cookie handing removed in favour for LocalStorage

--- index.php
+++ index.php
@@ -1,177 +1,154 @@
 <?php
 
-    $style = "light";
-    //	$style = "dark";
-    //	$style = "fancy";
-
-    //	$mode = "blue";
-    //	$mode = "grey";
-    //	$mode = "green";
-    //	$mode = "red";
-    //	$mode = "black";
-    //	$mode = "white";
-    //	$mode = "greenred";
-    $mode = "greenblack";
-
-    //	$mode = "blackwhite";
-    //	$mode = "whiteblack";
-
-    $fontsize = "small";
-    //	$fontsize = "medium";
-    //	$fontsize = "big";
-
-    function getDirColorHexString($cnt, $i)
-    {
-        $mode = $_COOKIE['gradient'];
-        if( $mode == '') $mode = 's';                
-        $ing = 256;
-        $step = floor($ing/$cnt);
-		
-        switch( $mode )
-        {
-			case "blue":
-        	    return '#0000FF';
-				break;
-			case "green":
-        	    return '#00FF00';					
-				break;
-			case "red":
-        	    return '#FF0000';
-				break;
-			default:	
-			case "black": 
-				return '#000000'; 
-				break;
-			case "grey": 
-				return '#a0a0a0'; 
-				break;
-			case "white": 
-				return '#FFFFFF'; 
-				break;
-			case "greenred":
-                if( $i < $cnt/2)
-                {
-	                (string)$val = dechex(($cnt-$i)*$step);
-    	            if( $i === 0 ) $val = 'FF';
-        	        if( strlen($val) === 1 ) $val = '0' . $val;
-            	    return '#00' . $val . '00';
-                }else
-                {
-	                (string)$val = dechex($i*$step);
-    	            if( $i === ($cnt-1) ) $val = 'FF';
-        	        if( strlen($val) === 1 ) $val = '0' . $val;
-            	    return '#' . $val . '0000';
-                }				
-				break;
-			case "greenblack":
-    	        (string)$val = dechex(($cnt-$i)*$step);
-				if( $i === 0 ) $val = 'FF';
-				if( $i === ($cnt-1) ) $val = '00';
-                if( strlen($val) === 1 ) $val = '0' . $val;
-                return '#00' . $val . '00';
-				break;
-			case "whiteblack":
-                (string)$val = dechex(($cnt-$i)*$step);
-				if( $i === 0 ) $val = 'FF';
-				if( $i === ($cnt-1) ) $val = '00';
-    	        if( strlen($val) === 1 ) $val = '0' . $val;
-        	    return '#' . $val . $val . $val;
-				break;
-			case "blackwhite":
-                (string)$val = dechex(($i)*$step);
-				if( $i === 0 ) $val = '00';
-				if( $i === ($cnt-1) ) $val = 'FF';
-    	        if( strlen($val) === 1 ) $val = '0' . $val;
-        	    return '#' . $val . $val . $val;
-				break;
-        }
-        
-        $step = floor($ing/$cnt);
-        if( $onlyGreen == false )
-        {
-        }else
-        {
-            (string)$val = dechex(($cnt-$i)*$step);
-			if( $i === 0 ) $val = 'FF';
-			if( $i === ($cnt-1) ) $val = '00';
-            if( strlen($val) === 1 ) $val = '0' . $val;
-            return '#00' . $val . '00';
-        }
-    }
-	
 	clearstatcache();
 	$dirs = array();
-    if ($handle = opendir(dirname(__FILE__))){
+    if ($handle = opendir($_SERVER['DOCUMENT_ROOT'])){
         while (false !== ($dir = readdir($handle))) {
             if( is_dir($dir) && substr($dir, 0, 1) != '.' ){
                 $pos = fileatime($dir);
                 do{
                     $pos--;
                 } while( array_key_exists($pos, $dirs ));
-                $dirs[$pos] = $dir;
+
+                $url = $dir;
+                if( is_dir( $dir . '/htdocs')){
+                    $url = $dir . '/htdocs';
+                }
+                $wp_admin = is_dir( $url . '/wp-admin') ? (' [<a href="' . $url . '/wp-admin">WP</a>]') : '';
+
+                $dirs[$pos] = array(
+                    'dir'  => $dir,
+                    'wp'   => $wp_admin,
+                    'mage' => '',
+                    'url'  => $url
+                );
             }
         }
     }
     closedir($handle);
     krsort($dirs);
 
-
-    if ( $_COOKIE['fontsize'] == '' )
-    {
-        $_COOKIE['fontsize'] = $fontsize;
-    }
-
 ?>
 <html>
     <head>
         <title>Auswahl</title>
+        <script type="text/javascript">
+
+            function setColor(color, id){
+                document.getElementById( id ).style.borderColor = color;
+            }
+
+            function setDirColorHexString(cnt, i, id)
+            {
+                var mode = get_value('mode');
+
+                var ing = 256;
+                var step = Math.floor(ing/cnt);
+
+                switch( mode )
+                {
+        			case "blue":
+                        setColor('#0000FF', id);
+        				break;
+        			case "green":
+                        setColor('#00FF00', id);
+        				break;
+        			case "red":
+                        setColor('#FF0000', id);
+        				break;
+        			default:
+        			case "black":
+                        setColor('#000000', id);
+        				break;
+        			case "grey":
+                        setColor('#a0a0a0', id);
+        				break;
+        			case "white":
+                        setColor('#FFFFFF', id);
+        				break;
+        			case "greenred":
+                        if( i < (cnt/2))
+                        {
+        	                var val = ((cnt-i)*step).toString(16);
+            	            if( i === 0 ) val = 'FF';
+                	        if( val.length === 1 ) val = '0' + val;
+                            setColor('#00' + val + '00', id);
+                        }else
+                        {
+        	                val = (i*step).toString(16);
+            	            if( i === (cnt-1) ) val = 'FF';
+                	        if( val.length === 1 ) val = '0' + val;
+                            setColor('#' + val + '0000', id);
+                        }
+        				break;
+        			case "greenblack":
+            	        var val = ((cnt-i)*step).toString(16);
+        				if( i === 0 ) val = 'FF';
+        				if( i === (cnt-1) ) val = '00';
+                        if( val.length === 1 ) val = '0' + val;
+                        setColor('#00' + val + '00', id);
+        				break;
+        			case "whiteblack":
+                        val = ((cnt-i)*step).toString(16);
+        				if( i === 0 ) val = 'FF';
+        				if( i === (cnt-1) ) val = '00';
+            	        if( val.length === 1 ) val = '0' + val;
+                	    setColor('#' + val + val + val, id);
+        				break;
+        			case "blackwhite":
+                        var val = ((i)*step).toString(16);
+        				if( i === 0 ) val = '00';
+        				if( i === (cnt-1) ) val = 'FF';
+            	        if( val.length === 1 ) val = '0' + val;
+                	    setColor('#' + val + val + val, id);
+                        break;
+                }
+            }
+
+            function set_value(key,value)
+            {
+                localStorage.setItem(key, value);
+            }
+
+            function get_value(key) {
+                return localStorage.getItem(key);
+            }
+
+            function delete_value(key) {
+                localStorage.removeItem(key);
+            }
+
+            if( get_value('style') == ''||get_value('style') == null ){
+                set_value('style', 'light' );
+            }
+
+            if( get_value('fontsize') == ''||get_value('fontsize') == null ){
+                set_value('fontsize', 'medium' );
+            }
+            if( get_value('mode') == ''||get_value('mode') == null ){
+                set_value('mode', 'greenblack' );
+            }
+            if( get_value('settings') == ''||get_value('settings') == null ){
+                set_value('settings', true );
+            }
+
+            var style    = get_value('style');
+            var fontsize = get_value('fontsize');
+            var mode     = get_value('mode');
+            var settings = get_value('settings');
+
+
+        </script>
     </head>
 
-    <body class="<?php echo $_COOKIE['fontsize']; ?>">
+    <body id="body" class="">
         <noscript>
             <link rel="stylesheet" href="./index.light.css" type="text/css">
         </noscript>
         <script type="text/javascript">
-			function set_cookie(key,value)
-			{
-				document.cookie = key+"="+value+";";
-			}
-
-			function read_cookie(key) {
-				var key_eq = key + "=";
-				var ca = document.cookie.split(';');
-				for(var i=0;i< ca.length;i++) {
-					var c = ca[i];
-					while (c.charAt(0)==' ') {
-						c = c.substring(1,c.length);
-					}
-					if (c.indexOf(key_eq) == 0) {
-						return c.substring(key_eq.length,c.length);
-					}
-				}
-				return null;
-			}
-
-			function delete_cookie(key) {
-				set_cookie(key,"",-1);
-			}        
-			
-			if( read_cookie('style') == '' ){
-				set_cookie('style', '<?php echo $style; ?>' );
-			}
-            if( read_cookie('fontsize') == '' ){
-                set_cookie('fontsize', '<?php echo $fontsize; ?>' );
-            }            
-			if( read_cookie('gradient') == '' ){
-				set_cookie('gradient', '<?php echo $mode; ?>');
-			}
-			var style = read_cookie('style');
-            if( style == null ) style = '<?php echo $style; ?>';
-            console.log( style );
-			document.write('<link rel="stylesheet" href="./index.' + style + '.css" type="text/css">');
-
+            document.write('<link rel="stylesheet" href="./index.' + style + '.css" type="text/css">');
         </script>
-        <div class="utility box" style="border-color: <?php echo getDirColorHexString(count($dirs), 0); ?>">
+        <div class="utility box" id="utility"">
             <h2>Helferlein</h2>
             <ul>
                 <li><a href="http://localhost/MAMP/phpinfo.php">phpInfo</a></li>
@@ -179,11 +156,13 @@
                 <li><a href="http://localhost/MAMP/phpmyadmin.php?lang=en-iso-8859-1&language=English">phpMyAdmin</a></li>
                 <li><a href="http://localhost/MAMP/English/faq.php?language=English">FAQ</a></li>
             </ul>
-            <?php if( $_COOKIE['fontsize'] != '' ) : ?>
+            <a onclick="set_value('settings', false);window.location.reload()" id="closeSettings" style="cursor: pointer">- Optionen</a>
+            <a onclick="set_value('settings', true);window.location.reload()" id="openSettings" style="cursor: pointer">+ Optionen</a>
+            <div id="settings" style="margin-top: 10px;">
                 <h3>Hintergund</h3>
                 <ul class="select">
                     <li>
-                        <select name="" onchange="if(this.value!=''){set_cookie('style', this.value);window.location.reload();}">
+                        <select name="" onchange="if(this.value!=''){set_value('style', this.value);window.location.reload();}">
                             <option value="">Bitte w&auml;hlen</option>
                             <option value="light">Hell</option>
                             <option value="dark">Dunkel</option>
@@ -194,7 +173,7 @@
                 <h3>Farben</h3>
                 <ul class="select">
                     <li>
-                        <select name="" onchange="if(this.value!=''){set_cookie('gradient', this.value);window.location.reload();}">
+                        <select name="" onchange="if(this.value!=''){set_value('mode', this.value);window.location.reload();}">
                             <option value="">Bitte w&auml;hlen</option>
                             <option value="blue">Blau</option>
                             <option value="grey">Grau</option>
@@ -212,7 +191,7 @@
                 <h3>Schriftgr&ouml;&szlig;e</h3>
                 <ul class="select">
                     <li>
-                        <select name="" onchange="if(this.value!=''){set_cookie('fontsize', this.value);window.location.reload();}">
+                        <select name="" onchange="if(this.value!=''){set_value('fontsize', this.value);window.location.reload();}">
                             <option value="">Bitte w&auml;hlen</option>
                             <option value="small">Klein</option>
                             <option value="medium">Mittel</option>
@@ -220,20 +199,38 @@
                         </select>
                     </li>
                 </ul>
-            <?php endif; ?>
+            </div>
         </div>
+
 		<?php
-            $i = 0;
-            foreach( $dirs AS $dir ){
-                $url = $dir;
-                if( is_dir( $dir . '/htdocs')){
-                    $url = $dir . '/htdocs';
-                }
-                $wp_admin = is_dir( $url . '/wp-admin') ? (' [<a href="' . $url . '/wp-admin">WP</a>]') : '';
-                $mage_admin = is_dir( $url . '/admin') ? (' [<a href="' . $url . '/admin">Mage</a>]') : '';
-                echo '<div class="box" style="border-color: ' . getDirColorHexString(count($dirs), $i) . '"><h2><a href="/' . $url . '">' . $dir . '</a>' . $wp_admin . '</h2></div>'."\n";
-                $i++;
+            $ids   = array();
+            $ids[] = 'utility';
+            foreach( $dirs AS $key=>$item ){
+                $ids[] = $key;
+                echo '<div class="box" id="' . $key . '"><h2><a href="/' . $item['url'] . '">' . $item['dir'] . '</a>' . $item['wp'] . '</h2></div>'."\n";
             }
         ?>
+        <script>
+            var dircnt = <?php echo count($dirs); ?>;
+            var ids    = new Array('<?php echo join("','", $ids); ?>');
+            init();
+
+            function init(){
+                if( get_value('settings') == "true"){
+                    document.getElementById("settings").style.display = 'block';
+                    document.getElementById("openSettings").style.display = 'none';
+                    document.getElementById("closeSettings").style.display = 'block';
+                }
+                else{
+                    document.getElementById("settings").style.display = 'none';
+                    document.getElementById("openSettings").style.display = 'block';
+                    document.getElementById("closeSettings").style.display = 'none';
+                }
+                document.getElementById("body").setAttribute("class", get_value('fontsize'));
+                for(i=0;i<ids.length;i++){
+                    setDirColorHexString(dircnt, i, ids[i]);
+                }
+            }
+        </script>
     </body>
 </html>
